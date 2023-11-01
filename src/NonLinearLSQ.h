0de567664f7629aed47b632e913d54b0be53d0fb
Alan <alanff15@gmail.com>
2023-10-31 23:11:54 -0300

    Template no .h

--- src/NonLinearLSQ.h
+++ src/NonLinearLSQ.h
@@ -2,30 +2,102 @@
 
 #include <Eigen/Eigen>
 
-#define ERROR_TYPE double
-#define VECTOR_TYPE Eigen::Matrix<ERROR_TYPE, -1, 1>
-#define MATRIX_TYPE Eigen::Matrix<ERROR_TYPE, -1, -1>
-
 #define DEFAULT_MAX_ITR 500
-#define DEFAULT_EPSILON (ERROR_TYPE)(1e-8)
-#define DEFAULT_DELTA (ERROR_TYPE)(1e-5)
+#define DEFAULT_EPSILON (1e-8)
+#define DEFAULT_DELTA (1e-5)
 
+template <typename NUM_TYP, uint32_t N>
 class NonLinearLSQ {
 public:
   NonLinearLSQ();
   ~NonLinearLSQ();
 
-  void setErrorFunction(const std::function<ERROR_TYPE(const VECTOR_TYPE&, const std::vector<ERROR_TYPE>&)>& func);
-  void setData(const std::vector<std::vector<ERROR_TYPE>>& data_in);
-  void solve(const VECTOR_TYPE& param_gess);
-  VECTOR_TYPE getParam();
+  void setErrorFunction(const std::function<NUM_TYP(const Eigen::Matrix<NUM_TYP, N, 1>&, const std::vector<NUM_TYP>&)>&);
+  void setData(const std::vector<std::vector<NUM_TYP>>&);
+  void solve(const Eigen::Matrix<NUM_TYP, N, 1>&);
+  Eigen::Matrix<NUM_TYP, N, 1> getParam();
 
 private:
-  ERROR_TYPE derror_dx(int dx_index, const VECTOR_TYPE& param, const std::vector<ERROR_TYPE>& data_line);
-  void getSystem(const VECTOR_TYPE& param, MATRIX_TYPE& H, VECTOR_TYPE& b);
+  NUM_TYP derror_dx(int, const Eigen::Matrix<NUM_TYP, N, 1>&, const std::vector<NUM_TYP>&);
+  void getSystem(const Eigen::Matrix<NUM_TYP, N, 1>&, Eigen::Matrix<NUM_TYP, N, N>&, Eigen::Matrix<NUM_TYP, N, 1>&);
 
-  VECTOR_TYPE delta;
-  VECTOR_TYPE final_param;
-  const std::vector<std::vector<ERROR_TYPE>>* data;
-  std::function<ERROR_TYPE(const VECTOR_TYPE&, const std::vector<ERROR_TYPE>&)> error;
+  Eigen::Matrix<NUM_TYP, N, 1> delta;
+  Eigen::Matrix<NUM_TYP, N, 1> final_param;
+  const std::vector<std::vector<NUM_TYP>>* data;
+  std::function<NUM_TYP(const Eigen::Matrix<NUM_TYP, N, 1>&, const std::vector<NUM_TYP>&)> error;
 };
+
+///////////////////////////////////////////////////////////////////////////////
+//   Implementation:   ////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename NUM_TYP, uint32_t N>
+NonLinearLSQ<NUM_TYP, N>::NonLinearLSQ() {}
+
+template <typename NUM_TYP, uint32_t N>
+NonLinearLSQ<NUM_TYP, N>::~NonLinearLSQ() {}
+
+template <typename NUM_TYP, uint32_t N>
+NUM_TYP NonLinearLSQ<NUM_TYP, N>::derror_dx(int dx_index, const Eigen::Matrix<NUM_TYP, N, 1>& input, const std::vector<NUM_TYP>& data_line) {
+  Eigen::Matrix<NUM_TYP, N, 1> input_delta = input;
+  input_delta[dx_index] += delta[dx_index];
+  return (error(input_delta, data_line) - error(input, data_line)) / delta[dx_index];
+}
+
+template <typename NUM_TYP, uint32_t N>
+void NonLinearLSQ<NUM_TYP, N>::getSystem(const Eigen::Matrix<NUM_TYP, N, 1>& input, Eigen::Matrix<NUM_TYP, N, N>& H, Eigen::Matrix<NUM_TYP, N, 1>& b) {
+  delta.resize(input.rows(), 1);
+  for (int i = 0; i < delta.rows(); i++) delta(i) = (NUM_TYP)DEFAULT_DELTA;
+  // zerar sistema
+  for (int j, i = 0; i < H.rows(); i++) {
+    for (j = 0; j < H.cols(); j++) {
+      H(i, j) = 0;
+    }
+    b(i) = 0;
+  }
+  // varer dados
+  Eigen::Matrix<NUM_TYP, N, N> Hs;
+  Eigen::Matrix<NUM_TYP, N, 1> bs;
+  Eigen::Matrix<NUM_TYP, N, 1> Jt;  // rows:IN_TYPE cols:OUT_TYPE
+  NUM_TYP err;
+  for (int j, i = 0; i < data->size(); i++) {
+    err = error(input, (*data)[i]);
+    for (j = 0; j < Jt.rows(); j++) {
+      Jt(j) = derror_dx(j, input, (*data)[i]);
+    }
+    Hs << Jt * Jt.transpose();
+    bs << Jt * err;
+    H += Hs;
+    b += bs;
+  }
+}
+
+template <typename NUM_TYP, uint32_t N>
+void NonLinearLSQ<NUM_TYP, N>::setErrorFunction(const std::function<NUM_TYP(const Eigen::Matrix<NUM_TYP, N, 1>&, const std::vector<NUM_TYP>&)>& func) {
+  error = func;
+}
+
+template <typename NUM_TYP, uint32_t N>
+void NonLinearLSQ<NUM_TYP, N>::setData(const std::vector<std::vector<NUM_TYP>>& data_in) {
+  data = &data_in;
+}
+
+template <typename NUM_TYP, uint32_t N>
+void NonLinearLSQ<NUM_TYP, N>::solve(const Eigen::Matrix<NUM_TYP, N, 1>& input_gess) {
+  Eigen::Matrix<NUM_TYP, N, 1> param = input_gess;
+  Eigen::Matrix<NUM_TYP, N, N> H;
+  Eigen::Matrix<NUM_TYP, N, 1> b;
+  Eigen::Matrix<NUM_TYP, N, 1> delta_param;
+  int itr = 1;
+  do {
+    getSystem(param, H, b);
+    delta_param = H.colPivHouseholderQr().solve(-b);
+    param += delta_param;
+  } while ((delta_param.norm() > (NUM_TYP)DEFAULT_EPSILON) && (++itr < DEFAULT_MAX_ITR));
+  final_param = param;
+}
+
+template <typename NUM_TYP, uint32_t N>
+Eigen::Matrix<NUM_TYP, N, 1> NonLinearLSQ<NUM_TYP, N>::getParam() {
+  return final_param;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,33 @@
 #include "NonLinearLSQ.h"
 
 int main() {
-  // dados: data[0] = 0.5 * data[1] * exp(-0.1 / data[2])
-  std::vector<std::vector<double>> data = {
-    {0.215165909, 0.541424337, 0.435451410},   //
-    {0.082343417, 0.499607021, 0.090108263},   //
-    {0.152911824, 0.356209865, 0.655688484},   //
-    {0.151307916, 0.345427797, 0.755746402},   //
-    {0.062072589, 0.141048351, 0.783385474},   //
-    {0.210911017, 0.538434625, 0.409697399},   //
-    {0.097431422, 0.243853868, 0.445884757},   //
-    {0.419880274, 0.958346544, 0.757044344},   //
-    {0.133205695, 0.468705683, 0.177012036},   //
-    {0.000427536, 0.001038838, 0.513685065},   //
-    {0.303274068, 0.869445960, 0.277722164}};  //
-  // inserir ruido nos dados
-  for (auto& d : data) {
-    d[0] += (double)(std::rand() % 100 - 50) / 10000.0;
-    d[1] += (double)(std::rand() % 100 - 50) / 10000.0;
-    d[2] += (double)(std::rand() % 100 - 50) / 10000.0;
-  }
+  // criar solver
+  NonLinearLSQ<float, 2> lsq;
 
-  // funcao de erro: y=f(x) -> err=(y-f(x))²
-  auto errorFunc = [](const Eigen::Vector2d& param, const std::vector<double>& yx) {
-    double ret = yx[0];                                // y
+  // inserir dados
+  std::vector<std::vector<float>> dados = {
+    {0.215165909f, 0.541424337f, 0.435451410f},
+    {0.082343417f, 0.499607021f, 0.090108263f},
+    {0.152911824f, 0.356209865f, 0.655688484f},
+    {0.151307916f, 0.345427797f, 0.755746402f},
+    {0.062072589f, 0.141048351f, 0.783385474f},
+    {0.210911017f, 0.538434625f, 0.409697399f},
+    {0.097431422f, 0.243853868f, 0.445884757f},
+    {0.419880274f, 0.958346544f, 0.757044344f},
+    {0.133205695f, 0.468705683f, 0.177012036f},
+    {0.000427536f, 0.001038838f, 0.513685065f},
+    {0.303274068f, 0.869445960f, 0.277722164f}};
+  lsq.setData(dados);
+
+  // definir funcao de erro: y = f(x) | err = (y-f(x))²
+  lsq.setErrorFunction([](const Eigen::Vector2f& param, const std::vector<float>& yx) {
+    float ret = yx[0];                                 // y
     ret -= param[0] * yx[1] * exp(-param[1] / yx[2]);  // f(x)
     return ret * ret;                                  // err
-  };
+  });
 
   // resolver
-  NonLinearLSQ lsq;
-  lsq.setData(data);
-  lsq.setErrorFunction(errorFunc);
-  lsq.solve(Eigen::Vector2d(0, 0));
+  lsq.solve(Eigen::Vector2f(0, 0));
 
   // mostrar resultado
   std::cout << "Ideal: 0.5\t0.1" << std::endl;
